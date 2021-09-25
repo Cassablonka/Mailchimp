cca8399fcc7c788a0eaa1324c9a6793a6e1f91b0
Cassablonka <cassapimpalapure@gmail.com>
2021-09-26 00:52:02 +0530

    Initial commits of the project.

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from mailchimp3 import MailChimp
+import os
+
+app = Flask(__name__)
+
+key = os.environ.get('API_KEY')
+list_id = os.environ.get('LIST_ID')
+
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        name = request.form['fullname']
+        email = request.form['email']
+        marks_10 = request.form['10th']
+        marks_12 = request.form['12th']
+        client = MailChimp(key, '{}-{}'.format('Cassa2006', 'us5'))
+        if client.lists.members.create(list_id, {'email_address': email, 'status': 'subscribed', 'merge_fields': {'FNAME': name, '10TH': marks_10, '12TH': marks_12,}}):
+            return render_template('success.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
