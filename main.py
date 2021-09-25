fb2ae2ca16a26f7c44ac533b42733e8f6cafa496
Cassablonka <cassapimpalapure@gmail.com>
2021-09-26 01:20:02 +0530

    Bug fixes.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def hello():
         email = request.form['email']
         marks_10 = request.form['10th']
         marks_12 = request.form['12th']
-        client = MailChimp('87c1c11c32ac8f96255a07744dafe4a0-us5', '{}-{}'.format('Cassa2006', 'us5'))
+        client = MailChimp('23f0c421cbd31005823751c05ebc89d7-us5', '{}-{}'.format('Cassa2006', 'us5'))
         if client.lists.members.create('84e91fce79', {'email_address': email, 'status': 'subscribed', 'merge_fields': {'FNAME': name, '10TH': marks_10, '12TH': marks_12,}}):
             return render_template('success.html')
     return render_template('index.html')
