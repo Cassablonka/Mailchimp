96ff8a3811c35c03ba6499bd2abbd9ff726bd78b
Cassablonka <cassapimpalapure@gmail.com>
2021-09-26 00:58:24 +0530

    Bug fixes.

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import os
 
 app = Flask(__name__)
 
-key = os.environ.get('API_KEY')
-list_id = os.environ.get('LIST_ID')
-
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     if request.method == 'POST':
@@ -14,6 +11,8 @@ def hello():
         email = request.form['email']
         marks_10 = request.form['10th']
         marks_12 = request.form['12th']
+        key = os.environ.get('API_KEY')
+        list_id = os.environ.get('LIST_ID')
         client = MailChimp(key, '{}-{}'.format('Cassa2006', 'us5'))
         if client.lists.members.create(list_id, {'email_address': email, 'status': 'subscribed', 'merge_fields': {'FNAME': name, '10TH': marks_10, '12TH': marks_12,}}):
             return render_template('success.html')
